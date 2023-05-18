bdd7a6235c6259800b901d0e251c9ebdb6ab74e3
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-05-18 14:16:54 +0300

    Import LoggerService and UserController

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
 import express, { Express } from 'express';
-import { userRouter } from './users/users.js';
-import { LoggerService } from './logger/logger.service.js';
 import { Server } from 'http';
+
+import { LoggerService } from './logger/logger.service.js';
+import { UserController } from './users/users.controller.js';
 
 export class App {
   app: Express;
   server: Server;
   port: number;
   logger: LoggerService;
+  userController: UserController;
 
-  constructor(logger: LoggerService) {
+  constructor(logger: LoggerService, userController: UserController) {
     this.app = express();
     this.port = 5000;
     this.logger = logger;
+    this.userController = userController;
   }
 
   useRoutes() {
-    this.app.use('/users', userRouter);
+    this.app.use('/users', this.userController.router);
   }
 
   public async init() {
