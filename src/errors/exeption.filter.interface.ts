5eb6d14045f947a24e179eacdda8a4f37042a6da
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-05-22 13:01:41 +0300

    Add interface IExeptionFilter for interface ExeptionFilter

---
+++ src/errors/exeption.filter.interface.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Response, Request } from 'express';
+
+export interface IExeptionFilter {
+  catch: (
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => void;
+}
