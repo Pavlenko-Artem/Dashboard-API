a4b8c50b7dbed6d78fee9fa8cd36ee3d5d154622
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-06-27 16:47:32 +0300

    UserComtrollers implements IUserController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,9 +5,10 @@ import { BaseController } from '../common/base.controller.js';
 import { HTTPError } from '../errors/http-error.class.js';
 import { TYPES } from '../types.js';
 import { ILogger } from '../logger/logger.interface.js';
+import { IUserController } from './users.controller.interface.js';
 
 @injectable()
-export class UserController extends BaseController {
+export class UserController extends BaseController implements IUserController {
   constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
     super(loggerService);
     this.bindRoutes([
