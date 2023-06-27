9ca7ad5505421522e0e607dfe601c5534826a60d
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-06-27 16:46:45 +0300

    Create IUserController

---
+++ src/users/users.controller.interface.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Response, Request } from 'express';
+
+export interface IUserController {
+  register: (req: Request, res: Response, next: NextFunction) => void;
+  login: (req: Request, res: Response, next: NextFunction) => void;
+}
