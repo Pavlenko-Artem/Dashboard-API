44fc4167c3a786ce27cf03b083d743b3e959eec3
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-07-06 22:28:42 +0300

    Добавлено получение salt через ConfigService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,19 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 
 import { IUserService } from './users.service.interface';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { User } from './user.entity';
 import { UserLoginDto } from './dto/user-login.dto';
+import { IConfigService } from '../config/config.service.interface';
+import { TYPES } from '../types';
 
 @injectable()
 export class UserService implements IUserService {
+	constructor(@inject(TYPES.IConfigService) private configService: IConfigService) {}
 	async createUser({ name, email, password }: UserRegisterDto): Promise<User | null> {
 		const newUser = new User(email, name);
-		await newUser.setPassword(password);
+		const salt = this.configService.get('SALT');
+		await newUser.setPassword(password, +salt);
 
 		return null;
 	}
